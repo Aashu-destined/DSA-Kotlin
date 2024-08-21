8e9674e872f02af695fc5f07737679267494aebf
Adarsh Sahu <sahuadarsh0@gmail.com>
2024-08-22 03:42:27 +0530

    Time: 204 ms (32.47%), Space: 37.5 MB (56.82%) - LeetHub

---
+++ 0078-subsets/0078-subsets.kt
@@ -0,0 +1,23 @@
+class Solution {
+    val finalArr : MutableList<List<Int>> = mutableListOf()
+    fun subsets(nums: IntArray): List<List<Int>> {
+        // send an empty array 
+        returnSubset(0,nums,mutableListOf())
+        return finalArr
+    }
+
+    fun returnSubset(i:Int,nums: IntArray,arr : MutableList<Int>){
+
+        if(i>=nums.size){
+        // save on base case
+        finalArr.add(arr.toList())
+        return
+        }
+        // add items to the array 
+        arr.add(nums[i])
+        returnSubset(i+1,nums,arr)
+        // return and remove from the array
+        arr.remove(nums[i])
+        returnSubset(i+1,nums,arr)
+    }
+}
